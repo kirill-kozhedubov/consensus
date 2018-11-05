3a1ed54fd895f1f343d1a4ff0ac661021c71837b
kirill-kozhedubov <veniq137@gmail.com>
2018-11-05 23:57:05 +0200

    major db implementation

---
+++ src/main/java/iq/ven/portal/consensus/controllers/kanbanboard/create/payload/KanbanBoardCreationRequest.java
@@ -0,0 +1,11 @@
+package iq.ven.portal.consensus.controllers.kanbanboard.create.payload;
+
+public class KanbanBoardCreationRequest {
+
+    private String name;
+
+    private String description;
+
+}
+
+
--- src/main/java/iq/ven/portal/consensus/controllers/project/payload/ProjectCreationRequest.java
+++ src/main/java/iq/ven/portal/consensus/controllers/project/payload/ProjectCreationRequest.java
@@ -1,4 +1,9 @@
 package iq.ven.portal.consensus.controllers.project.payload;
 
 public class ProjectCreationRequest {
+
+    private String name;
+
+    private String description;
+
 }
