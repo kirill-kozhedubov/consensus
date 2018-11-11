6574d24c3650ac069a0d17ab7cb2d56e2477ab0f
kirill-kozhedubov <veniq137@gmail.com>
2018-11-11 16:31:45 +0200

    request

--- src/main/java/iq/ven/portal/consensus/controllers/issues/attachments/IssueAttachmentsViewController.java
+++ src/main/java/iq/ven/portal/consensus/controllers/issues/attachments/IssueAttachmentsViewController.java
@@ -1,4 +1,11 @@
 package iq.ven.portal.consensus.controllers.issues.attachments;
 
-public class IssueAttachmentsViewController {
+import iq.ven.portal.consensus.controllers.AbstractController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IssueAttachmentsViewController extends AbstractController {
+
+    private static final Logger logger = LoggerFactory.getLogger(IssueAttachmentsViewController.class);
+
 }
--- src/main/java/iq/ven/portal/consensus/controllers/issues/create/payload/CreateIssueRequest.java
+++ src/main/java/iq/ven/portal/consensus/controllers/issues/create/payload/CreateIssueRequest.java
@@ -1,4 +1,55 @@
 package iq.ven.portal.consensus.controllers.issues.create.payload;
 
 public class CreateIssueRequest {
+
+    private String name;
+
+    private String description;
+
+    private String project;
+
+    private String assignee;
+
+    private String boardId;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public String getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(String boardId) {
+        this.boardId = boardId;
+    }
 }
